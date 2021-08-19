19e79b2720bf6f86bf4e25aadcf4bee782e73a23
CroxyRieS <81052764+CroxyRieS@users.noreply.github.com>
2021-08-19 13:52:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
+fs.readdir('./commands/', (err, files) => {
     if (err) console.error(err);
     files.forEach(f => {
-        let props = require(`./komutlar/${f}`);
+        let props = require(`./commands/${f}`);
         client.commands.set(props.help.name, props);
         props.conf.aliases.forEach(alias => {
             client.aliases.set(alias, props.help.name);
@@ -18,8 +18,8 @@ fs.readdir('./komutlar/', (err, files) => {
 client.reload = command => {
     return new Promise((resolve, reject) => {
         try {
-            delete require.cache[require.resolve(`./komutlar/${command}`)];
-            let cmd = require(`./komutlar/${command}`);
+            delete require.cache[require.resolve(`./commands/${command}`)];
+            let cmd = require(`./commands/${command}`);
             client.commands.delete(command);
             client.aliases.forEach((cmd, alias) => {
                 if (cmd === command) client.aliases.delete(alias);
@@ -37,7 +37,7 @@ client.reload = command => {
 client.load = command => {
     return new Promise((resolve, reject) => {
         try {
-            let cmd = require(`./komutlar/${command}`);
+            let cmd = require(`./commamds/${command}`);
             client.commands.set(command, cmd);
             cmd.conf.aliases.forEach(alias => {
                 client.aliases.set(alias, cmd.help.name);
@@ -51,8 +51,8 @@ client.load = command => {
 client.unload = command => {
     return new Promise((resolve, reject) => {
         try {
-            delete require.cache[require.resolve(`./komutlar/${command}`)];
-            let cmd = require(`./komutlar/${command}`);
+            delete require.cache[require.resolve(`./commands/${command}`)];
+            let cmd = require(`./commands/${command}`);
             client.commands.delete(command);
             client.aliases.forEach((cmd, alias) => {
                 if (cmd === command) client.aliases.delete(alias);
